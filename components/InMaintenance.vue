96c1ba02fc96d250ed8df41b384e33192994c9a7
vincejs77 <vinjobs77@gmail.com>
2022-11-14 17:11:48 +0200

    style(in_maintain): edit 003

--- components/InMaintenance.vue
+++ components/InMaintenance.vue
@@ -8,25 +8,31 @@
       class="bg-d_inmaintain__overlay absolute w-full top-0 right-0 left-0 bottom-0"
     ></div>
 
-    <div class="max-w-lg">
-      <div class="flex justify-left items-center w-full">
+    <div class="max-w-lg text-center">
+      <div
+        class="border-b-2 pb-10 border-b-gray border-opacity-20 flex justify-center items-center w-full"
+      >
         <span class="hidden sm:block">
           <UtilsLogo :w="40" />
         </span>
         <span class="block sm:hidden">
           <UtilsLogo :w="30" />
         </span>
-        <span class="ml-4 font-wotfardBold text-2xl mt-1.5 text-gray"> 243digit </span>
+        <span class="ml-4 font-wotfardBold text-xl mt-1.5 text-gray"> 243digit </span>
       </div>
       <h1 class="font-wotfardBold text-4xl sm:text-6xl mt-8 sm:mt-16 relative">
         Nous sommes en
-        <span class="text-white">construction</span>
+        <span class="text-white bg__txt_blue">construction</span>
       </h1>
       <div class="mt-10">
         <p class="text-lg sm:text-xl mt-6 sm:mt-12">
-          <span class="text-white">Nous travaillons sur notre site web.</span> Vous pouvez
-          vous inscrire à notre liste de diffusion pour etre informer dès le lancement.
+          <span class="text-white">Nous travaillons sur notre site web.</span> Il sera en
+          ligne sous peu. En attendant, contactez-nous en cas de besoin.
         </p>
+      </div>
+
+      <div class="bg-blackContrast text-white mt-16 rounded-md min-w-full p-8 relative">
+        <span class="font-wotfardBold">1d</span>
       </div>
     </div>
   </div>
